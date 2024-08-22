dc574b46dec54a60df8f59358976c3e42e30992c
Jason Hargrove <rijaha@gmail.com>
2024-08-21 22:16:17 -0500

    Added a HeroSection

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './App.css'
 import Header from './components/Header'
+import HeroSection from './components/HeroSection'
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <Header />
+      <HeroSection />
     </div>
   )
 }
---
+++ src/components/HeroSection.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const HeroSection: React.FC = () => {
+  return (
+    <section>
+      <div>
+        <h1>Welcome to the Nuclear Graffiti Store</h1>
+      </div>
+    </section>
+  )
+}
+
+export default HeroSection
