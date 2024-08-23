841324d5878eefcd804fda91ecd484e046a77afb
Jason Hargrove <rijaha@gmail.com>
2024-08-22 23:37:40 -0500

    Added more content to the hero section

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -2,9 +2,16 @@ import React from 'react'
 
 const HeroSection: React.FC = () => {
   return (
-    <section>
-      <div>
-        <h1>Welcome to the Nuclear Graffiti Store</h1>
+    <section className="bg-gray-100">
+      <div className="max-w-7xl mx-auto py-16 px-4 sm:px-6 lg:px-8">
+        <div className="text-center">
+          <h1 className="text-4xl font-bold text-gray-900">
+            Welcome to the Nuclear Graffiti Store
+          </h1>
+          <p className="mt-4 text-lg leading-6 text-gray-500">
+            Explore our collection of graffiti images with nuclear themes
+          </p>
+        </div>
       </div>
     </section>
   )
